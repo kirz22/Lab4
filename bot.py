a41145abb918f5f1963c960ada11bc738b9566b8
kirz22 <108638160+kirz22@users.noreply.github.com>
2024-03-15 04:24:17 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import telebot
 bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(content_types=["text"])
-def repeat_all_messages(message): # Название функции не играет никакой роли
+def repeat_all_messages(message):
     bot.send_message(message.chat.id, message.text)
 
 if __name__ == '__main__':
